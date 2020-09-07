34ae782f914bc92e13aefeaf11845e33b3d5a18a
Tiberius <mercytiberius@yahoo.com>
2020-09-07 16:03:59 +0300

    fix

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Card, CardBody, CardTitle, CardText, CardImg } from "reactstrap";
+
 class DishDetail extends Component {
   constructor(props) {
     super(props);
