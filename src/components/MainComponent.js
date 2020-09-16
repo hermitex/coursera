3a19711e7dea2b24beb874c1db2007b7c52fab9e
Tiberius <mercytiberius@yahoo.com>
2020-09-17 00:42:04 +0300

    Single Page Application Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,9 @@ import Menu from "../components/MenuComponent";
 import DishDetail from "../components/DishDetailComponent";
 import Contact from "./ContactComponent";
 import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { LEADERS } from "../shared/leaders";
+import { PROMOTIONS } from "../shared/promotions";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import { Switch, Route, Redirect } from "react-router-dom";
@@ -13,6 +16,9 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
 
@@ -20,7 +26,13 @@ class Main extends Component {
     const HomePage = () => {
       return (
         <div>
-          <Home />
+          <Home
+            dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+            promotion={
+              this.state.promotions.filter((promo) => promo.featured)[0]
+            }
+            leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
         </div>
       );
     };
