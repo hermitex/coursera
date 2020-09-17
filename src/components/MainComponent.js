67024674809fa40e282fedb5209a8b08b61876db
Tiberius <mercytiberius@yahoo.com>
2020-09-18 00:46:31 +0300

    finish exercise 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import { PROMOTIONS } from "../shared/promotions";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import { Switch, Route, Redirect } from "react-router-dom";
+import About from "./AboutComponent";
 
 class Main extends Component {
   constructor(props) {
@@ -57,6 +58,11 @@ class Main extends Component {
         <Header></Header>
         <Switch>
           <Route path="/home" component={HomePage} />
+          <Route
+            exact
+            path="/aboutus"
+            component={() => <About leaders={this.state.leaders} />}
+          />
           <Route
             exact
             path="/menu"
