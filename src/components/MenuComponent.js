2fdad70267ee74927b8f02005a06057d926da586
Tiberius <mercytiberius@yahoo.com>
2020-09-17 16:18:20 +0300

    Single Page Applications Part 2

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,15 +1,26 @@
 import React from "react";
-import { Card, CardImg, CardImgOverlay, CardBody, CardTitle } from "reactstrap";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardBody,
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
 const RenderMenuItem = ({ dish, onClick }) => {
   return (
-    <Card onClick={() => onClick(dish.id)}>
-      <CardImg width="100%" src={dish.image} alt={dish.name} />
-      <CardImgOverlay>
-        <CardBody className="ml-5">
-          <CardTitle>{dish.name}</CardTitle>
-        </CardBody>
-      </CardImgOverlay>
+    <Card>
+      <Link to={`/menu/${dish.id}`}>
+        <CardImg width="100%" src={dish.image} alt={dish.name} />
+        <CardImgOverlay>
+          <CardBody className="ml-5">
+            <CardTitle>{dish.name}</CardTitle>
+          </CardBody>
+        </CardImgOverlay>
+      </Link>
     </Card>
   );
 };
@@ -18,13 +29,27 @@ const Menu = (props) => {
   const menu = props.dishes.map((dish) => {
     return (
       <div key={dish.id} className="col-12 col-md-5 m-1">
-        <RenderMenuItem dish={dish} onClick={props.onClick}></RenderMenuItem>
+        <RenderMenuItem dish={dish}></RenderMenuItem>
       </div>
     );
   });
 
   return (
     <div className="container">
+      <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to="/home">Home</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem active>
+            <Link to="/menu">Menu</Link>
+          </BreadcrumbItem>
+        </Breadcrumb>
+        <div className="col-12">
+          <h3>Menu</h3>
+          <hr />
+        </div>
+      </div>
       <div className="row">{menu}</div>
     </div>
   );
